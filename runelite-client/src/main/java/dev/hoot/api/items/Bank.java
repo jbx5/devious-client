7c2624ca1fce6f2377570a66871f10bc6a39a1c6
Alan Baumgartner <alanvbaumgartner@gmail.com>
2022-01-01 11:45:15 -0600

    Add vararg contains and count to bank

--- runelite-client/src/main/java/dev/hoot/api/items/Bank.java
+++ runelite-client/src/main/java/dev/hoot/api/items/Bank.java
@@ -396,9 +396,44 @@ public class Bank extends Items
 		return BANK.exists(id);
 	}
 
+	public static boolean contains(int... id)
+	{
+		return BANK.exists(id);
+	}
+
 	public static boolean contains(String name)
 	{
 		return BANK.exists(name);
+	}
+
+	public static boolean contains(String... name)
+	{
+		return BANK.exists(name);
+	}
+
+	public static int count(Predicate<Item> filter)
+	{
+		return getAll(filter).stream().mapToInt(Item::getQuantity).sum();
+	}
+
+	public static int count(int id)
+	{
+		return getAll(id).stream().mapToInt(Item::getQuantity).sum();
+	}
+
+	public static int count(int... id)
+	{
+		return getAll(id).stream().mapToInt(Item::getQuantity).sum();
+	}
+
+	public static int count(String name)
+	{
+		return getAll(name).stream().mapToInt(Item::getQuantity).sum();
+	}
+
+	public static int count(String... name)
+	{
+		return getAll(name).stream().mapToInt(Item::getQuantity).sum();
 	}
 
 	public static List<Widget> getTabs()
