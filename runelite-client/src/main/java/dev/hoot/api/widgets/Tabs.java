322614787c473e4a11c1c8a3a6ea85cf146ffd3a
Yuri Moens <yuri.moens@gmail.com>
2022-02-24 13:53:45 +0100

    Add method for opening tabs with client script

--- runelite-client/src/main/java/dev/hoot/api/widgets/Tabs.java
+++ runelite-client/src/main/java/dev/hoot/api/widgets/Tabs.java
@@ -1,6 +1,9 @@
 package dev.hoot.api.widgets;
 
+import dev.hoot.api.game.Game;
+import dev.hoot.api.game.GameThread;
 import dev.hoot.api.game.Vars;
+import net.runelite.api.GameState;
 import net.runelite.api.VarClientInt;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetID;
@@ -16,6 +19,16 @@ public class Tabs
 		{
 			widget.interact(0);
 		}
+	}
+
+	public static void openInterface(Tab tab)
+	{
+		if (Game.getClient() == null || Game.getState() != GameState.LOGGED_IN)
+		{
+			return;
+		}
+
+		GameThread.invoke(() -> Game.getClient().runScript(915, tab.getIndex()));
 	}
 
 	public static boolean isOpen(Tab tab)
