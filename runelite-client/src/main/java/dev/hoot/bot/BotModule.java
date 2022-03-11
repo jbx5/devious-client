4d78b8596078b49387da6dc8691f717a8616b6a6
Yuri Moens <yuri.moens@gmail.com>
2022-03-11 21:33:18 +0100

    api: remove KeyManager binding

--- runelite-client/src/main/java/dev/hoot/bot/BotModule.java
+++ runelite-client/src/main/java/dev/hoot/bot/BotModule.java
@@ -25,7 +25,6 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.config.RuneLiteConfig;
 import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.game.ItemManager;
-import net.runelite.client.input.KeyManager;
 import net.runelite.client.menus.MenuManager;
 import net.runelite.client.task.Scheduler;
 import net.runelite.client.util.DeferredEventBus;
@@ -83,7 +82,6 @@ public class BotModule extends AbstractModule
 		bind(ChatMessageManager.class);
 		bind(ItemManager.class);
 		bind(Scheduler.class);
-		bind(KeyManager.class);
 
 		bind(Gson.class).toInstance(RuneLiteAPI.GSON);
 
