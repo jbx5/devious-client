2d6c35fa658c405ba58caf250c89271d3ef2a5dc
Burak <burakinan1@hotmail.com>
2022-05-01 00:51:01 +0200

    project: checkstyle

--- runelite-client/src/main/java/dev/unethicalite/api/plugins/SettingsPlugin.java
+++ runelite-client/src/main/java/dev/unethicalite/api/plugins/SettingsPlugin.java
@@ -1,13 +1,7 @@
 package dev.unethicalite.api.plugins;
 
 import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.plugins.Plugin;
-import net.runelite.client.plugins.config.PluginConfigurationDescriptor;
-import net.runelite.client.plugins.config.PluginListPanel;
-
-import javax.inject.Inject;
-import javax.inject.Provider;
 
 public abstract class SettingsPlugin extends Plugin
 {
