80cd7869774bd63c9565c0189e98d1cd741c0b20
Burak <burakinan1@hotmail.com>
2022-05-26 15:20:21 +0200

    mouse: join monitors when mouse forwarding v2

--- runelite-client/src/main/java/dev/unethicalite/managers/interaction/InteractionManager.java
+++ runelite-client/src/main/java/dev/unethicalite/managers/interaction/InteractionManager.java
@@ -5,14 +5,11 @@ import dev.unethicalite.api.SceneEntity;
 import dev.unethicalite.api.commons.Rand;
 import dev.unethicalite.api.commons.Time;
 import dev.unethicalite.api.events.MenuAutomated;
-import dev.unethicalite.api.events.NativeKeyInput;
-import dev.unethicalite.api.events.NativeMouseInput;
 import dev.unethicalite.api.game.GameThread;
 import dev.unethicalite.api.input.naturalmouse.NaturalMouse;
 import dev.unethicalite.api.packets.Packets;
 import dev.unethicalite.api.widgets.Widgets;
 import dev.unethicalite.client.config.UnethicaliteConfig;
-import dev.unethicalite.client.minimal.ui.MinimalUI;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.DialogOption;
@@ -25,15 +22,10 @@ import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.ui.ClientUI;
-import net.runelite.client.ui.ContainableFrame;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.awt.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Singleton
 @Slf4j
@@ -195,189 +187,6 @@ public class InteractionManager
 		}
 	}
 
-	@Subscribe
-	public void onNativeMouseInput(NativeMouseInput event)
-	{
-		if (!canForwardMouseEvent(event))
-		{
-			return;
-		}
-
-		MouseHandler mouseHandler = client.getMouseHandler();
-		if (mouseHandler == null)
-		{
-			return;
-		}
-
-		double eventX = event.getX();
-		double eventY = event.getY();
-
-		ContainableFrame frame = MinimalUI.getFrame() != null ? MinimalUI.getFrame() : ClientUI.getFrame();
-
-		if (frame == null)
-		{
-			return;
-		}
-
-		if (frame.getBounds().contains(eventX, eventY))
-		{
-			return;
-		}
-
-		List<GraphicsDevice> monitors = Arrays.asList(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices());
-		GraphicsDevice currentMonitor = monitors.stream()
-				.filter(device -> device.getDefaultConfiguration().getBounds().contains(event.getX(), event.getY()))
-				.findFirst()
-				.orElse(null);
-		if (currentMonitor == null)
-		{
-			return;
-		}
-
-		List<Integer> monitorIds = Arrays.stream(config.selectedMonitorIds().split(","))
-				.map(String::trim)
-				.map(Integer::parseInt)
-				.collect(Collectors.toList());
-
-		double screenHeight = currentMonitor.getDisplayMode().getHeight();
-
-		double trueEventX = eventX;
-		double totalWidth = 0;
-		double totalCutWidth = 0;
-		double firstMonitorWidth = 0;
-		for (int i = 0; i < monitors.size(); i++)
-		{
-			final double width = monitors.get(i).getDisplayMode().getWidth();
-			if (config.selectedMonitorsOnly() && !monitorIds.contains(i + 1))
-			{
-				totalCutWidth += width;
-			}
-			else if (monitorIds.contains(i + 1) && firstMonitorWidth == 0)
-			{
-				firstMonitorWidth = width;
-			}
-
-			totalWidth += width;
-		}
-
-		trueEventX += firstMonitorWidth;
-		if (trueEventX > totalCutWidth)
-		{
-			trueEventX -= totalCutWidth;
-		}
-
-		eventX = trueEventX / (totalWidth - totalCutWidth);
-
-		if (eventY < 0)
-		{
-			eventY = screenHeight + eventY;
-		}
-		else if (eventY > screenHeight)
-		{
-			eventY = eventY - screenHeight;
-		}
-
-		int canvasX = (int) (client.getCanvasWidth() * eventX);
-		int canvasY = (int) (eventY * ((double) client.getCanvasHeight() / screenHeight));
-
-		int currentMonitorId = monitors.indexOf(currentMonitor) + 1;
-		if (config.selectedMonitorsOnly() && !monitorIds.contains(currentMonitorId))
-		{
-			eventX = canvasX = -1;
-			eventY = canvasY = -1;
-		}
-
-		if (event.getType() != NativeMouseInput.Type.MOVEMENT && eventX == -1 && eventY == -1)
-		{
-			if (client.isFocused())
-			{
-				log.debug("Setting client unfocused");
-				mouseHandler.sendFocusLost();
-			}
-
-			return;
-		}
-
-		MenuAutomated queuedMenu = client.getQueuedMenu();
-
-		if (event.getType() == NativeMouseInput.Type.PRESS
-				&& queuedMenu != null
-				&& queuedMenu.getOpcode() != MenuAction.WALK
-				&& clickInsideMinimap(new Point(canvasX, canvasY)))
-		{
-			return;
-		}
-
-		int finalEventX = (int) eventX;
-		int finalEventY = (int) eventY;
-		int finalCanvasX = canvasX;
-		int finalCanvasY = canvasY;
-
-		switch (event.getType())
-		{
-			case PRESS:
-				int button = config.forwardLeftClick() ? 1 : event.getButton();
-
-				if (queuedMenu == null)
-				{
-					if (config.forceForwarding())
-					{
-						mouseHandler.sendClick(canvasX, canvasY, button);
-					}
-
-					return;
-				}
-
-				GameThread.invoke(() ->
-				{
-					if (button == 1)
-					{
-						client.setPendingAutomation(queuedMenu);
-						setHoveredEntity(queuedMenu.getEntity());
-						client.setQueuedMenu(null);
-					}
-
-					log.debug(
-							"Forwarding mouse press [{}] from [{}, {}, {}] to canvas [{}, {}]",
-							button,
-							currentMonitor.getIDstring(),
-							finalEventX,
-							finalEventY,
-							finalCanvasX,
-							finalCanvasY
-					);
-
-					mouseHandler.sendClick(finalCanvasX, finalCanvasY, button);
-				});
-
-				break;
-
-			case RELEASE:
-				mouseHandler.sendRelease();
-				break;
-
-			case MOVEMENT:
-				mouseHandler.sendMovement(canvasX, canvasY);
-				break;
-		}
-	}
-
-	@Subscribe
-	public void onNativeKeyInput(NativeKeyInput event)
-	{
-		if (config.forwardKeystrokes())
-		{
-			Point mouse = MouseInfo.getPointerInfo().getLocation();
-			client.getCallbacks().post(
-					new NativeMouseInput(
-							mouse.x,
-							mouse.y,
-							1,
-							NativeMouseInput.Type.PRESS
-					)
-			);
-		}
-	}
 
 	@Subscribe
 	public void onDialogProcessed(DialogProcessed e)
@@ -398,7 +207,7 @@ public class InteractionManager
 		}
 	}
 
-	private void setHoveredEntity(SceneEntity entity)
+	public void setHoveredEntity(SceneEntity entity)
 	{
 		if (entity != null)
 		{
@@ -449,7 +258,7 @@ public class InteractionManager
 		return randomPoint;
 	}
 
-	private boolean clickInsideMinimap(Point point)
+	public boolean clickInsideMinimap(Point point)
 	{
 		return getMinimap().contains(point);
 	}
@@ -482,25 +291,5 @@ public class InteractionManager
 	{
 		return point.x < 0 || point.y < 0
 				|| point.x > client.getViewportWidth() || point.y > client.getViewportHeight();
-	}
-
-	private boolean canForwardMouseEvent(NativeMouseInput event)
-	{
-		if (config.interactMethod() != InteractMethod.MOUSE_FORWARDING)
-		{
-			return false;
-		}
-
-		if (config.forceForwardMovement() && event.getType() == NativeMouseInput.Type.MOVEMENT)
-		{
-			return true;
-		}
-
-		if (config.forceForwarding())
-		{
-			return true;
-		}
-
-		return client.getQueuedMenu() != null;
 	}
 }
