3193f0ceb30546609a9301264a8a0219e5a2b77e
Burak <burakinan1@hotmail.com>
2022-06-08 10:09:30 +0200

    api: add timeout for connecting with backend

--- runelite-client/src/main/java/net/unethicalite/api/movement/pathfinder/TransportLoader.java
+++ runelite-client/src/main/java/net/unethicalite/api/movement/pathfinder/TransportLoader.java
@@ -3,6 +3,7 @@ package net.unethicalite.api.movement.pathfinder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.runelite.api.*;
+import net.unethicalite.api.commons.HttpUtil;
 import net.unethicalite.api.entities.NPCs;
 import net.unethicalite.api.entities.Players;
 import net.unethicalite.api.entities.TileObjects;
@@ -20,9 +21,6 @@ import net.unethicalite.api.widgets.Dialog;
 import net.unethicalite.api.widgets.Widgets;
 import net.unethicalite.client.Static;
 import net.unethicalite.client.minimal.config.UnethicaliteProperties;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -54,39 +52,29 @@ public class TransportLoader
 		new MagicMushtree(new WorldPoint(3676, 3755, 0), WidgetInfo.FOSSIL_MUSHROOM_SWAMP),
 		new MagicMushtree(new WorldPoint(3760, 3758, 0), WidgetInfo.FOSSIL_MUSHROOM_VALLEY)
 	);
-	private static final Gson GSON = new GsonBuilder().create();
 	private static int LAST_BUILD_TICK = 0;
-	private static final List<Transport> STATIC_TRANSPORTS = new ArrayList<>();
+	private static final List<Transport> STATIC_TRANSPORTS;
 	private static final WorldArea MLM = new WorldArea(3714, 5633, 60, 62, 0);
 	private static List<Transport> LAST_TRANSPORT_LIST = Collections.emptyList();
 
 	static
 	{
-		// Try to initialize the static transports before usage
-		loadStaticTransports();
-	}
+		STATIC_TRANSPORTS = new ArrayList<>();
+		TransportDto[] dtos = HttpUtil.readFromUrl(UnethicaliteProperties.getApiUrl() + "/transports",
+				TransportDto[].class);
 
-	private static List<Transport> loadStaticTransports()
-	{
-		if (!STATIC_TRANSPORTS.isEmpty())
+		if (dtos != null)
 		{
-			return STATIC_TRANSPORTS;
-		}
-
-		try (InputStream txt = new URL(UnethicaliteProperties.getApiUrl() + "/transports").openStream())
-		{
-			TransportDto[] json = GSON.fromJson(new String(txt.readAllBytes()), TransportDto[].class);
-
-			for (TransportDto transportDto : json)
+			log.debug("Loaded {} transports", dtos.length);
+			for (TransportDto dto : dtos)
 			{
-				STATIC_TRANSPORTS.add(transportDto.toModel());
+				STATIC_TRANSPORTS.add(dto.toModel());
 			}
 		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+	}
 
+	private static List<Transport> getStaticTransports()
+	{
 		return STATIC_TRANSPORTS;
 	}
 
@@ -333,7 +321,7 @@ public class TransportLoader
 
 	public static List<Transport> buildCachedTransportList()
 	{
-		List<Transport> transports = new ArrayList<>(loadStaticTransports());
+		List<Transport> transports = new ArrayList<>(getStaticTransports());
 
 		// Entrana
 		transports.add(npcTransport(new WorldPoint(3041, 3237, 0), new WorldPoint(2834, 3331, 1), 1166, "Take-boat"));
