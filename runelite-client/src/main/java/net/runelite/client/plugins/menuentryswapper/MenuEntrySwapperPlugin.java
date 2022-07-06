47d61c141226fc33b12d1f0a44d274099b646cf7
Adam <Adam@sigterm.info>
2022-07-06 18:38:26 -0400

    menu swapper: prioritize custom swaps over hint arrow
    The hint arrow on the mole causes custom swaps like walk-here to not be applied
    
    (cherry picked from commit ec9b3dfb84d1a13552721e7e2108f87977b70400)

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -1111,15 +1111,6 @@ public class MenuEntrySwapperPlugin extends Plugin
 		final MenuAction menuAction = menuEntry.getType();
 		final String option = Text.removeTags(menuEntry.getOption()).toLowerCase();
 		final String target = Text.removeTags(menuEntry.getTarget()).toLowerCase();
-		final NPC hintArrowNpc = client.getHintArrowNpc();
-
-		// Don't swap on hint arrow npcs, usually they need "Talk-to" for clues.
-		if (hintArrowNpc != null
-			&& hintArrowNpc.getIndex() == eventId
-			&& NPC_MENU_TYPES.contains(menuAction))
-		{
-			return;
-		}
 
 		final boolean itemOp = menuEntry.isItemOp();
 		// Custom shift-click item swap
@@ -1267,6 +1258,16 @@ public class MenuEntrySwapperPlugin extends Plugin
 		}
 
 		if (swapBank(menuEntry, menuAction))
+		{
+			return;
+		}
+
+		final NPC hintArrowNpc = client.getHintArrowNpc();
+
+		// Don't swap on hint arrow npcs, usually they need "Talk-to" for clues.
+		if (hintArrowNpc != null
+			&& hintArrowNpc.getIndex() == eventId
+			&& NPC_MENU_TYPES.contains(menuAction))
 		{
 			return;
 		}
