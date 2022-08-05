13c63ef20971a9b0b81e62fada4b8f75394529a0
Burak <burakinan1@hotmail.com>
2022-08-05 18:19:36 +0200

    api: fix casting non-targeted spells

--- runelite-client/src/main/java/net/unethicalite/api/magic/Magic.java
+++ runelite-client/src/main/java/net/unethicalite/api/magic/Magic.java
@@ -81,7 +81,7 @@ public class Magic
 		Widget widget = Widgets.get(spell.getWidget());
 		if (widget != null)
 		{
-			widget.interact(0, MenuAction.CC_OP.getId());
+			widget.interact(1, MenuAction.CC_OP.getId());
 		}
 	}
 
