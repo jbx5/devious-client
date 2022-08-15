a8a93b6a24308c8e58e3547ab868cd02df74d072
Hoodz <laurens123987@gmail.com>
2022-08-15 13:45:03 +0200

    Update TeleportLoader.java
    - added some requirement checks to some teleports
    - added generic method to enter house (supports house tabs now)
    - added nexus portal handler
    
    to do:
    - add nexus teleports to config
    - ignore equipment option

--- runelite-client/src/main/java/net/unethicalite/api/movement/pathfinder/TeleportLoader.java
+++ runelite-client/src/main/java/net/unethicalite/api/movement/pathfinder/TeleportLoader.java
@@ -1,7 +1,14 @@
 package net.unethicalite.api.movement.pathfinder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import net.runelite.api.GameState;
 import net.runelite.api.Item;
+import net.runelite.api.ItemID;
 import net.runelite.api.ObjectID;
 import net.runelite.api.Quest;
 import net.runelite.api.TileObject;
@@ -20,6 +27,8 @@ import net.unethicalite.api.items.Equipment;
 import net.unethicalite.api.items.Inventory;
 import net.unethicalite.api.magic.Magic;
 import net.unethicalite.api.magic.Spell;
+import net.unethicalite.api.magic.SpellBook;
+import static net.unethicalite.api.movement.pathfinder.model.MovementConstants.*;
 import net.unethicalite.api.movement.pathfinder.model.Teleport;
 import net.unethicalite.api.movement.pathfinder.model.TeleportItem;
 import net.unethicalite.api.movement.pathfinder.model.TeleportSpell;
@@ -29,12 +38,6 @@ import net.unethicalite.api.widgets.Minigames;
 import net.unethicalite.api.widgets.Widgets;
 import net.unethicalite.client.Static;
 import net.unethicalite.client.managers.RegionManager;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
-import static net.unethicalite.api.movement.pathfinder.model.MovementConstants.*;
 
 public class TeleportLoader
 {
@@ -189,8 +192,11 @@ public class TeleportLoader
 								() -> jewelryTeleport("Digsite", DIGSITE_PENDANT)));
 						teleports.add(new Teleport(new WorldPoint(3764, 3869, 1), 6,
 								() -> jewelryTeleport("Fossil Island", DIGSITE_PENDANT)));
-						teleports.add(new Teleport(new WorldPoint(3549, 10456, 0), 6,
+						if (Quests.isFinished(Quest.DRAGON_SLAYER_II))
+						{
+							teleports.add(new Teleport(new WorldPoint(3549, 10456, 0), 6,
 								() -> jewelryTeleport("Lithkren", DIGSITE_PENDANT)));
+						}
 					}
 				}
 
@@ -276,75 +282,100 @@ public class TeleportLoader
 								() -> slayerRingTeleport("Fremennik Slayer Dungeon", SLAYER_RING)));
 						teleports.add(new Teleport(new WorldPoint(3185, 4601, 0), 2,
 								() -> slayerRingTeleport("Tarn's Lair", SLAYER_RING)));
-						teleports.add(new Teleport(new WorldPoint(2028, 4636, 0), 2,
+						if (Quests.isFinished(Quest.MOURNINGS_END_PART_II))
+						{
+							teleports.add(new Teleport(new WorldPoint(2028, 4636, 0), 2,
 								() -> slayerRingTeleport("Dark Beasts", SLAYER_RING)));
+						}
+					}
+				}
+
+				if (RegionManager.usePoh() && (canEnterHouse() || TileObjects.getNearest(ObjectID.PORTAL_4525) != null))
+				{
+					if (RegionManager.hasMountedGlory())
+					{
+						teleports.add(mountedPohTeleport(new WorldPoint(3087, 3496, 0), ObjectID.AMULET_OF_GLORY, "Edgeville"));
+						teleports.add(mountedPohTeleport(new WorldPoint(2918, 3176, 0), ObjectID.AMULET_OF_GLORY, "Karamja"));
+						teleports.add(mountedPohTeleport(new WorldPoint(3105, 3251, 0), ObjectID.AMULET_OF_GLORY, "Draynor Village"));
+						teleports.add(mountedPohTeleport(new WorldPoint(3293, 3163, 0), ObjectID.AMULET_OF_GLORY, "Al Kharid"));
+					}
+
+					if (RegionManager.hasMountedDigsitePendant())
+					{
+						teleports.add(pohDigsitePendantTeleport(new WorldPoint(3341, 3445, 0), 1));
+						teleports.add(pohDigsitePendantTeleport(new WorldPoint(3766, 3870, 1), 2));
+						if (Quests.isFinished(Quest.DRAGON_SLAYER_II))
+						{
+							teleports.add(pohDigsitePendantTeleport(new WorldPoint(3549, 10456, 0), 3));
+						}
+					}
+
+					switch (RegionManager.hasJewelryBox())
+					{
+						case ORNATE:
+							if (Quests.isFinished(Quest.THRONE_OF_MISCELLANIA))
+							{
+								teleports.add(pohWidgetTeleport(new WorldPoint(2538, 3863, 0), 'j'));
+							}
+							teleports.add(pohWidgetTeleport(new WorldPoint(3163, 3478, 0), 'k'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2996, 3375, 0), 'l'));
+							if (Quests.isFinished(Quest.BETWEEN_A_ROCK))
+							{
+								teleports.add(pohWidgetTeleport(new WorldPoint(2828, 10166, 0), 'm'));
+							}
+							teleports.add(pohWidgetTeleport(new WorldPoint(3087, 3496, 0), 'n'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2918, 3176, 0), 'o'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3105, 3251, 0), 'p'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3293, 3163, 0), 'q'));
+						case FANCY:
+							teleports.add(pohWidgetTeleport(new WorldPoint(2882, 3548, 0), '9'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3191, 3367, 0), 'a'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3052, 3488, 0), 'b'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2655, 3441, 0), 'c'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2611, 3390, 0), 'd'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3050, 9763, 0), 'e'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2933, 3295, 0), 'f'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3143, 3440, 0), 'g'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(1662, 3505, 0), 'h'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(1249, 3718, 0), 'i'));
+						case BASIC:
+							teleports.add(pohWidgetTeleport(new WorldPoint(3315, 3235, 0), '1'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2440, 3090, 0), '2'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3151, 3635, 0), '3'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2898, 3553, 0), '4'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2520, 3571, 0), '5'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(2964, 4382, 2), '6'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(3244, 9501, 2), '7'));
+							teleports.add(pohWidgetTeleport(new WorldPoint(1624, 3938, 0), '8'));
+							break;
+						default:
 					}
 				}
 			}
 
-
-
-			if (RegionManager.usePoh() && (TeleportSpell.TELEPORT_TO_HOUSE.canCast() || TileObjects.getNearest(ObjectID.PORTAL_4525) != null))
-			{
-				if (RegionManager.hasMountedGlory())
-				{
-					teleports.add(mountedPohTeleport(new WorldPoint(3087, 3496, 0), ObjectID.AMULET_OF_GLORY, "Edgeville"));
-					teleports.add(mountedPohTeleport(new WorldPoint(2918, 3176, 0), ObjectID.AMULET_OF_GLORY, "Karamja"));
-					teleports.add(mountedPohTeleport(new WorldPoint(3105, 3251, 0), ObjectID.AMULET_OF_GLORY, "Draynor Village"));
-					teleports.add(mountedPohTeleport(new WorldPoint(3293, 3163, 0), ObjectID.AMULET_OF_GLORY, "Al Kharid"));
-				}
-
-				if (RegionManager.hasMountedDigsitePendant())
-				{
-					teleports.add(pohDigsitePendantTeleport(new WorldPoint(3341, 3445, 0), 1));
-					teleports.add(pohDigsitePendantTeleport(new WorldPoint(3766, 3870, 1), 2));
-					teleports.add(pohDigsitePendantTeleport(new WorldPoint(3549, 10456, 0), 3));
-				}
-
-				switch (RegionManager.hasJewelryBox())
-				{
-					case ORNATE:
-						if (Quests.isFinished(Quest.THRONE_OF_MISCELLANIA))
-						{
-							teleports.add(pohWidgetTeleport(new WorldPoint(2538, 3863, 0), 'j'));
-						}
-						teleports.add(pohWidgetTeleport(new WorldPoint(3163, 3478, 0), 'k'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2996, 3375, 0), 'l'));
-						if (Quests.isFinished(Quest.BETWEEN_A_ROCK))
-						{
-							teleports.add(pohWidgetTeleport(new WorldPoint(2828, 10166, 0), 'm'));
-						}
-						teleports.add(pohWidgetTeleport(new WorldPoint(3087, 3496, 0), 'n'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2918, 3176, 0), 'o'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3105, 3251, 0), 'p'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3293, 3163, 0), 'q'));
-					case FANCY:
-						teleports.add(pohWidgetTeleport(new WorldPoint(2882, 3548, 0), '9'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3191, 3367, 0), 'a'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3052, 3488, 0), 'b'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2655, 3441, 0), 'c'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2611, 3390, 0), 'd'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3050, 9763, 0), 'e'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2933, 3295, 0), 'f'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3143, 3440, 0), 'g'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(1662, 3505, 0), 'h'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(1249, 3718, 0), 'i'));
-					case BASIC:
-						teleports.add(pohWidgetTeleport(new WorldPoint(3315, 3235, 0), '1'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2440, 3090, 0), '2'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3151, 3635, 0), '3'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2898, 3553, 0), '4'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2520, 3571, 0), '5'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(2964, 4382, 2), '6'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(3244, 9501, 2), '7'));
-						teleports.add(pohWidgetTeleport(new WorldPoint(1624, 3938, 0), '8'));
-						break;
-					default:
-				}
-			}
 			LAST_TELEPORT_LIST.clear();
 			LAST_TELEPORT_LIST.addAll(teleports);
 		});
+	}
+
+	public static boolean canEnterHouse()
+	{
+		return Inventory.contains(ItemID.TELEPORT_TO_HOUSE) || TeleportSpell.TELEPORT_TO_HOUSE.canCast();
+	}
+
+	public static void enterHouse()
+	{
+		if (TeleportSpell.TELEPORT_TO_HOUSE.canCast())
+		{
+			SpellBook.Standard.TELEPORT_TO_HOUSE.cast();
+			return;
+		}
+
+		Item teleTab = Inventory.getFirst(ItemID.TELEPORT_TO_HOUSE);
+		if (teleTab != null)
+		{
+			teleTab.interact("Break");
+		}
 	}
 
 	public static void jewelryTeleport(String target, int... ids)
@@ -368,6 +399,54 @@ public class TeleportLoader
 		{
 			equipped.interact(target);
 		}
+	}
+
+	public static Teleport pohNexusTeleport(WorldPoint destination, String target)
+	{
+		return new Teleport(destination, 10, () ->
+		{
+			if (!Players.getLocal().isIdle() || Static.getClient().getGameState() == GameState.LOADING)
+			{
+				return;
+			}
+
+			TileObject nexusPortal = TileObjects.getNearest("Portal Nexus");
+			if (nexusPortal == null)
+			{
+				enterHouse();
+				return;
+			}
+
+			Widget teleportInterface = Widgets.get(17, 12);
+			if (teleportInterface == null || teleportInterface.isHidden())
+			{
+				nexusPortal.interact("Teleport Menu");
+				return;
+			}
+
+			Widget[] teleportChildren = teleportInterface.getDynamicChildren();
+			if (teleportChildren == null || teleportChildren.length == 0)
+			{
+				//no teleports in the portal
+				return;
+			}
+
+			Optional<Widget> optionalTeleportWidget = Arrays.stream(teleportChildren).
+				filter(Objects::nonNull).
+				filter(widget -> widget.getText() != null).
+				filter(widget -> widget.getText().contains(target)).
+				findFirst();
+
+			if (optionalTeleportWidget.isEmpty())
+			{
+				//the teleport is not in the list
+				return;
+			}
+
+			Widget teleportWidget = optionalTeleportWidget.get();
+			String teleportChar = teleportWidget.getText().substring(12, 13);
+			Keyboard.type(teleportChar);
+		});
 	}
 
 	public static void jewelryPopupTeleport(String target, int... ids)
@@ -432,8 +511,7 @@ public class TeleportLoader
 				return;
 			}
 
-			Magic.cast(TeleportSpell.TELEPORT_TO_HOUSE.getSpell());
-
+			enterHouse();
 		});
 	}
 
@@ -474,7 +552,8 @@ public class TeleportLoader
 				box.interact("Teleport Menu");
 				return;
 			}
-			Magic.cast(TeleportSpell.TELEPORT_TO_HOUSE.getSpell());
+
+			enterHouse();
 		});
 	}
 
@@ -497,7 +576,8 @@ public class TeleportLoader
 				first.interact(action);
 				return;
 			}
-			Magic.cast(TeleportSpell.TELEPORT_TO_HOUSE.getSpell());
+
+			enterHouse();
 		});
 	}
 
