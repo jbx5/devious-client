553d938c5fdb0d47c1e55946807107acae75b2b7
Burak <burakinan1@hotmail.com>
2022-08-22 14:21:46 +0200

    Update Dependencies.kt

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "1.0.0"
-    const val rlVersion = "1.8.29"
+    const val rlVersion = "1.8.31"
 
     const val openosrsVersion = "4.31.2"
 
