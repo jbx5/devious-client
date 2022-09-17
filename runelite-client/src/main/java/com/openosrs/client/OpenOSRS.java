ae14fcceeed08c946afd798d0d9202e3c0eb55a7
melxin <melxin@tutamail.com>
2022-09-18 00:05:02 +0200

    client: use rl login block data

--- runelite-client/src/main/java/com/openosrs/client/OpenOSRS.java
+++ runelite-client/src/main/java/com/openosrs/client/OpenOSRS.java
@@ -3,12 +3,15 @@ package com.openosrs.client;
 import com.google.common.base.Strings;
 import lombok.AccessLevel;
 import lombok.Getter;
-
+import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.Properties;
 import java.util.UUID;
 
+@Slf4j
 public class OpenOSRS
 {
 	public static final File OPENOSRS_DIR = new File(System.getProperty("user.home"), ".openosrs");
@@ -53,5 +56,22 @@ public class OpenOSRS
 
 	public static void preload()
 	{
+		/**
+		 * Use rl login block data
+		 *
+		 * This uses the rl login block instead of the myloginBlock byte array stored in random.dat
+		 */
+		//byte[] myLoginBlock = new byte[] {-124, -92, -35, -7, 38, 12, 74, -4, -34, -16, 116, 56, 9, -68, 50, 84, -32, -93, -104, 92, -128, -40, -50, -56};
+		final byte[] rlLoginBlock = new byte[] {-35, -91, -50, -47, -12, 24, 52, -102, 123, -32, 118, 92, -77, 48, 100, 12, -52, -79, -106, 34, -102, 91, -19, 121};
+		final File randomDatFile = new File(OPENOSRS_DIR + File.separator + "random.dat");
+		try
+		{
+			Files.write(randomDatFile.toPath(), rlLoginBlock, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+			log.info("Successfully wrote rl login block {} to {}", rlLoginBlock, randomDatFile.getAbsolutePath());
+		}
+		catch (IOException e)
+		{
+			log.error("Failed to write rl login block data to: {}", randomDatFile, e);
+		}
 	}
 }
