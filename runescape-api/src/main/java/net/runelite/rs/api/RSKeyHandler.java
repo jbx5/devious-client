c740ee5b6e33e9ddf2642e292dd801039e443a2e
melxin <melxin@tutamail.com>
2022-10-12 22:56:35 +0200

    rs-api: RSKeyHandler remove KeyFocusListener
    This is an addition to commit 371ca3526b38a80b25528a821715e9dd2cca3f90

--- runescape-api/src/main/java/net/runelite/rs/api/RSKeyHandler.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSKeyHandler.java
@@ -1,12 +1,11 @@
 package net.runelite.rs.api;
 
-import net.runelite.api.KeyFocusListener;
 import net.runelite.mapping.Import;
 
 import java.awt.event.FocusListener;
 import java.awt.event.KeyListener;
 
-public interface RSKeyHandler extends KeyListener, FocusListener, KeyFocusListener
+public interface RSKeyHandler extends KeyListener, FocusListener
 {
 	@Import("KeyHandler_idleCycles")
 	void setIdleCycles(int cycles);
