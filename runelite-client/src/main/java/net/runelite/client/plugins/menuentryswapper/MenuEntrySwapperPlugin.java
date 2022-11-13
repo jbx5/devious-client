5904ed04cabbe9f20e0568749d7f4cfdb2a07b45
Adam <adam@sigterm.info>
2022-11-13 02:13:08 +0100

    api: add post menu sort event
    Use post menu sort event for menu entry swapper, which allows swapping
    op5 on objects. Previously op5 could not be swapped due to the client
    sorting the menu after the client tick event ran.
    
    The post menu sort event runs in between menu sorting and the click
    handling code.
    
    It is still necessary to keep the rewrite of cc op low priority to cc op
    due to the client treating low priority ops as always having to right
    click.
    
    (cherry picked from commit 3fd1ba69500b3cd14ece4e1163e9e7a428d10935)

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -57,7 +57,6 @@ import net.runelite.api.NPC;
 import net.runelite.api.NPCComposition;
 import net.runelite.api.ObjectComposition;
 import net.runelite.api.ParamID;
-import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.MenuOpened;
 import net.runelite.api.events.PostItemComposition;
 import net.runelite.api.events.PostMenuSort;
@@ -520,6 +519,13 @@ public class MenuEntrySwapperPlugin extends Plugin
 				{
 					if (Strings.isNullOrEmpty(actions[actionIdx]))
 					{
+						continue;
+					}
+
+					if ("Build".equals(actions[actionIdx])
+						|| "Remove".equals(actions[actionIdx]))
+					{
+						// https://secure.runescape.com/m=news/third-party-client-guidelines?oldschool=1
 						continue;
 					}
 
@@ -1551,7 +1557,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onClientTick(ClientTick clientTick)
+	public void onPostMenuSort(PostMenuSort postMenuSort)
 	{
 		if (client.isMenuOpen())
 		{
