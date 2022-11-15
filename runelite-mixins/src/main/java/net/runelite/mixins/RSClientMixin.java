77e56aa1518c9e38febfbb2bb31a67b4b49ed39d
melxin <melxin@tutamail.com>
2022-11-15 16:19:24 +0100

    mixins: add post client tick event
    This is an addition to commit 5995a8d5c3e53a666d6601b0ed5e75681a885919

--- runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSClientMixin.java
@@ -2972,6 +2972,13 @@ public abstract class RSClientMixin implements RSClient
 	}
 
 	@Inject
+	@MethodHook(value = "doCycle", end = true)
+	protected final void doCycleEnd()
+	{
+		client.getCallbacks().tickEnd();
+	}
+
+	@Inject
 	public static void check(String name, RSEvictingDualNodeHashTable dualNodeHashTable)
 	{
 		boolean var3 = dualNodeHashTable.isTrashing();
