d9df66f806f18022bd9a5ca98e7a3bab2c255842
jbx5 <113217895+jbx5@users.noreply.github.com>
2023-05-27 14:58:16 -0700

    rlversion

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "1.0.0"
-    const val rlVersion = "1.10.0"
+    const val rlVersion = "1.10.1"
 
     const val openosrsVersion = "4.31.2"
 
