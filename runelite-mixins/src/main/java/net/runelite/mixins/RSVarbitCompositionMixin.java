449a249bf64f52debf472b3ef09e94b58634a67b
jbx5 <113217895+jbx5@users.noreply.github.com>
2023-06-02 13:36:08 -0700

    some fixes

--- runelite-mixins/src/main/java/net/runelite/mixins/RSVarbitCompositionMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSVarbitCompositionMixin.java
@@ -14,11 +14,19 @@ public abstract class RSVarbitCompositionMixin implements RSVarbitComposition
 	@Shadow("client")
 	private static RSClient client;
 
-	@MethodHook(value = "<clinit>", end = true)
+	/*@MethodHook(value = "<clinit>", end = true)
 	@Inject
 	public static void rl$clinit()
 	{
 		RSEvictingDualNodeHashTable varbitCache = client.getVarbitCache();
 		varbitCache.resize(256);
+	}*/
+
+	@MethodHook(value = "<init>", end = true)
+	@Inject
+	public void rl$init()
+	{
+		RSEvictingDualNodeHashTable cachedModels2 = client.getItemCompositionCache();
+		cachedModels2.resize(1024);
 	}
 }
