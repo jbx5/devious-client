0c8daa3ce6aa04610ccd8dd693791c2c5aa616ea
CommaDotComma <commadotcomma69@gmail.com>
2023-06-05 19:11:31 -0700

    Off-set needed to be on North-East tile

--- runelite-mixins/src/main/java/net/unethicalite/mixins/HGameObjectMixin.java
+++ runelite-mixins/src/main/java/net/unethicalite/mixins/HGameObjectMixin.java
@@ -40,10 +40,10 @@ public abstract class HGameObjectMixin implements RSGameObject
 				getLocalLocation().getY() + (sizeY() - 1) * Perspective.LOCAL_TILE_SIZE / 2
 		);
 
-		// Off-set south-west tile by -1 because WorldArea construct will add 1 to each
+		// Off-set north-east tile by -1 because WorldArea construct will add 1 setting width and height
 		return new WorldArea(
-				WorldPoint.fromLocal(client, localSWTile).dx(-1).dy(-1),
-				WorldPoint.fromLocal(client, localNETile)
+				WorldPoint.fromLocal(client, localSWTile),
+				WorldPoint.fromLocal(client, localNETile).dx(-1).dy(-1)
 		);
 	}
 
