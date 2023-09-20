147295a817fa9aac7096f1ddcd540a561a725654
jbx5 <113217895+jbx5@users.noreply.github.com>
2023-09-20 10:34:57 -0700

    bump project version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -34,5 +34,5 @@ object ProjectVersions {
 
     const val lombokVersion = "1.18.22"
 
-    const val unethicaliteVersion = "1.0.19-EXPERIMENTAL"
+    const val unethicaliteVersion = "1.0.20-EXPERIMENTAL"
 }
