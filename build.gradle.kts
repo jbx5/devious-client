54714aaebf987d102bec7490ed4b58f28edfb58d
melxin <melxin@tutamail.com>
2023-11-04 19:33:18 +0100

    project: Bump script-assembler version

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ buildscript {
     }
     dependencies {
         classpath("org.ajoberstar.grgit:grgit-core:4.1.0")
-        classpath("com.openosrs:script-assembler-plugin:1.0.1")
+        classpath("com.openosrs:script-assembler-plugin:1.0.2")
         classpath("com.openosrs:injector-plugin:2.0.18")
     }
 }
