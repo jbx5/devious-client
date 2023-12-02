12fb8c624db83211a52f543ae4edca35edeb455b
Jorgsaa <jorgen.saanum@gmail.com>
2023-12-02 16:00:17 +0100

    unethical: Added blocking event BankTutorialEvent for closing the bank tutorial

--- runelite-client/src/main/java/net/unethicalite/api/items/Bank.java
+++ runelite-client/src/main/java/net/unethicalite/api/items/Bank.java
@@ -1,5 +1,9 @@
 package net.unethicalite.api.items;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import net.runelite.api.InventoryID;
 import net.runelite.api.Item;
 import net.runelite.api.Varbits;
@@ -13,11 +17,6 @@ import net.unethicalite.api.query.items.ItemQuery;
 import net.unethicalite.api.widgets.Dialog;
 import net.unethicalite.api.widgets.Widgets;
 import net.unethicalite.client.Static;
-
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class Bank extends Items
 {
@@ -33,8 +32,6 @@ public class Bank extends Items
 	private static final Bank BANK = new Bank();
 	private static final Inventory BANK_INVENTORY = new Inventory();
 
-	private static final int WITHDRAW_MODE_VARBIT = 3958;
-	private static final int QUANTITY_MODE_VARP = 6590;
 	private static final Supplier<Widget> TAB_CONTAINER = () -> Widgets.get(WidgetInfo.BANK_TAB_CONTAINER);
 	private static final Supplier<Widget> BANK_CAPACITY = () -> Widgets.get(WidgetID.BANK_GROUP_ID, 9);
 	private static final Supplier<Widget> RELEASE_PLACEHOLDERS = () -> Widgets.get(WidgetID.BANK_GROUP_ID, 56);
@@ -321,7 +318,7 @@ public class Bank extends Items
 
 	public static boolean isNotedWithdrawMode()
 	{
-		return Vars.getBit(WITHDRAW_MODE_VARBIT) == 1;
+		return Vars.getBit(Varbits.BANK_WITHDRAW_MODE) == 1;
 	}
 
 	public static List<Item> getAll(Predicate<Item> filter)
@@ -551,6 +548,7 @@ public class Bank extends Items
 		{
 			return BANK_INVENTORY.count(false, names);
 		}
+
 		public static int getFreeSlots()
 		{
 			return 28 - getAll().size();
@@ -639,7 +637,7 @@ public class Bank extends Items
 
 		public static QuantityMode getCurrent()
 		{
-			switch (Vars.getBit(QUANTITY_MODE_VARP))
+			switch (Vars.getBit(Varbits.BANK_QUANTITY_MODE))
 			{
 				case 0:
 					return QuantityMode.ONE;
