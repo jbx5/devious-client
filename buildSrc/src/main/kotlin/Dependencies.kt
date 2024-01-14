7e14ae19ed77e2bb3de30967499bc32a411843c3
melxin <melxin@tutamail.com>
2024-01-14 16:20:06 +0100

    project: Bump RL version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object ProjectVersions {
     const val launcherVersion = "1.0.0"
-    const val rlVersion = "1.10.19"
+    const val rlVersion = "1.10.20"
 
     const val openosrsVersion = "4.31.2"
 
