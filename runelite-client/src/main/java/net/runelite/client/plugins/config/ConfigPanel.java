0d439297044132861ba5ecae86c3a2e62e18e803
Adam <Adam@sigterm.info>
2024-01-14 15:11:17 -0500

    config: use default font for JPasswordField
    The default font for this is now applied by flatlaf to be the rs font. Set it back to Dialog font so it doesn't rely on font fallback anymore. See ca56ef10823aea1ecd275c0d3c3f9a3b17838ff9
    
    (cherry picked from commit 39127c852263813b9763dc4d360683655a41ef15)

--- runelite-client/src/main/java/net/runelite/client/plugins/config/ConfigPanel.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/config/ConfigPanel.java
@@ -800,6 +800,7 @@ class ConfigPanel extends PluginPanel
 		if (cid.getItem().secret())
 		{
 			textField = new JPasswordField();
+			textField.setFont(FontManager.getDefaultFont());
 		}
 		else
 		{
