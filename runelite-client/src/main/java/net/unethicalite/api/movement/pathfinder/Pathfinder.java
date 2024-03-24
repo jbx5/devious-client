c3d612ac41f55490b959b9417a97e0fa2308cb3e
jmlundeen <jmlundee@yahoo.com>
2024-03-23 23:25:27 -0500

    Remove IgnoredDoor
    - IgnoredDoor no longer needed with collision map update

--- runelite-client/src/main/java/net/unethicalite/api/movement/pathfinder/Pathfinder.java
+++ runelite-client/src/main/java/net/unethicalite/api/movement/pathfinder/Pathfinder.java
@@ -32,7 +32,6 @@ public class Pathfinder implements Callable<List<WorldPoint>>
 {
 	final CollisionMap map;
 	final Map<WorldPoint, List<Transport>> transports;
-	private Map<WorldPoint, IgnoredDoor> ignoredDoors;
 	private List<Node> start;
 	private WorldArea target;
 	private List<WorldPoint> targetTiles;
@@ -75,7 +74,6 @@ public class Pathfinder implements Callable<List<WorldPoint>>
 		this.transports = transports;
 		this.target = target;
 		this.targetTiles = target.toWorldPointList();
-		this.ignoredDoors = IgnoredDoorLoader.getIgnoredDoors();
 		this.start = new ArrayList<>();
 		this.nearest = null;
 		this.avoidWilderness = avoidWilderness;
@@ -162,13 +160,6 @@ public class Pathfinder implements Callable<List<WorldPoint>>
 		if (avoidWilderness && isInWilderness(neighbor) && !isInWilderness(node.position) && !targetsInWilderness)
 		{
 			return;
-		}
-		if (ignoredDoors.getOrDefault(neighbor, null) != null)
-		{
-			if (ignoredDoors.get(neighbor).getRequirements().fulfilled())
-			{
-				return;
-			}
 		}
 		int cost = CharterShipLocation.getCharterShipCost(node.position, neighbor, ringOfCharosEquipped);
 		if (useCharterShips && cost > node.goldAvailable)
