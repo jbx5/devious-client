219a0de78bf76d87ed36f31b31c6007e284232d1
Adam <Adam@sigterm.info>
2024-10-02 11:27:08 -0400

    devtools: add tile flag devtool
    (cherry picked from commit db484aceb95e8520f5e15c7cbddb590fb18e7ff3)

--- runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPlugin.java
@@ -167,7 +167,7 @@ public class DevToolsPlugin extends Plugin
 	private DevToolsButton soundEffects;
 	private DevToolsButton scriptInspector;
 	private DevToolsButton inventoryInspector;
-	private DevToolsButton roofs;
+	private DevToolsButton tileFlags;
 	private DevToolsButton shell;
 	private DevToolsButton menus;
 	private DevToolsButton uiDefaultsInspector;
@@ -270,7 +270,7 @@ public class DevToolsPlugin extends Plugin
 		soundEffects = new DevToolsButton("Sound Effects");
 		scriptInspector = new DevToolsButton("Script Inspector");
 		inventoryInspector = new DevToolsButton("Inventory Inspector");
-		roofs = new DevToolsButton("Roofs");
+		tileFlags = new DevToolsButton("Tile flags");
 		shell = new DevToolsButton("Shell");
 		menus = new DevToolsButton("Menus");
 
